59b4c7a0821352d6d2ed8b502111eeb6c6612dd8
silveroxides <ishimarukaito@gmail.com>
2025-09-11 23:21:51 +0200

    exclude img_in_patch and nerf final

--- src/llama.cpp
+++ src/llama.cpp
@@ -18888,11 +18888,13 @@ static void llama_model_quantize_internal(const std::string & fname_inp, const s
             quantize &= name != "distilled_guidance_layer.out_proj.weight";
             quantize &= name != "distilled_guidance_layer.in_proj.weight";
             quantize &= name != "nerf_image_embedder.embedder.0.weight";
+            quantize &= name != "img_in_patch.weight";
             quantize &= name != "nerf_blocks.0.param_generator.weight";
             quantize &= name != "nerf_blocks.1.param_generator.weight";
             quantize &= name != "nerf_blocks.2.param_generator.weight";
             quantize &= name != "nerf_blocks.3.param_generator.weight";
             quantize &= name != "nerf_final_layer.linear.weight";
+            quantize &= name != "nerf_final_layer_conv.conv.weight";
         }
         if (model.arch == LLM_ARCH_SD1 || model.arch == LLM_ARCH_SDXL) {
             image_model = true;
